cee1ff91110f4dcbb84a3124db12ba94b0c11c98
Julita Inca <jinca@pucp.pe>
2017-09-15 16:25:59 -0500

    A comment is printing from an object converted to a string

--- introjs/obString.js
+++ introjs/obString.js
@@ -1,12 +1,22 @@
 let obj = {
-    name: 'Veronica',
-    age: 32,
-    comment:[
-        'Best friend ever',
-	'yes, you are',
-	'since I have met you'
-    ]
+	name: 'Veronica',
+	age: 22,
+	comment: [
+		'It was a very awesome trip',
+		'More than 21 miles you have walked',
+		{
+			name: 'Micaela'
+		}
+	]
 };
-myObj = obj['comment'][2];
-let stringObj = JSON.stringify(myObj);
-console.log(stringObj);
+
+theComment1 = obj['comment'][0];
+theComment2 = obj['comment'][1];
+nameComment = obj['comment'][2].name;
+
+let stringObj1 = JSON.stringify(theComment1);
+let stringObj2 = JSON.stringify(theComment2);
+let stringObj3 = JSON.stringify(nameComment);
+
+console.log("The comment "+stringObj1+" and "+stringObj2+" was done by "+stringObj3);
+
