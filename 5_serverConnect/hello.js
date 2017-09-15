703249fcebe8632770dfe04fb15381b63388e7d1
Julita Inca <jinca@pucp.pe>
2017-09-15 17:21:19 -0500

    Connecting HTTP server using different formats

---
+++ 5_serverConnect/hello.js
@@ -0,0 +1,13 @@
+const http = require("http");
+const hostname = "127.0.0.1";
+const port = 8888;
+
+const server = http.createServer((req, res) => {
+    res.write("Hello World!");
+    res.end();
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
